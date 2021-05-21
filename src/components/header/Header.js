de32efcd8ccc8cef8b46d783a915e7cf256ab37a
zahirekrem09 <zhrekrmsrtk@gmail.com>
2021-05-21 13:16:09 +0300

    initial commit

---
+++ src/components/header/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./header.css";
+
+function Header() {
+  return (
+    <div className="header">
+      <h2>Header</h2>
+    </div>
+  );
+}
+
+export default Header;
