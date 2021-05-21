55b6d3b696506ce42df69556b6bf452836ff27e9
zahirekrem09 <zhrekrmsrtk@gmail.com>
2021-05-21 14:25:46 +0300

    added header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,10 +1,42 @@
 import React from "react";
+import { IconButton, Avatar } from "@material-ui/core";
+import {
+  Menu as MenuIcon,
+  Search as SearchIcon,
+  ArrowDropDown,
+  Apps as AppsIcon,
+  Notifications,
+} from "@material-ui/icons";
+
 import "./header.css";
 
 function Header() {
   return (
     <div className="header">
-      <h2>Header</h2>
+      <div className="header__left">
+        <IconButton>
+          <MenuIcon />
+        </IconButton>
+        <img
+          src="https://ssl.gstatic.com/ui/v1/icons/mail/rfr/logo_gmail_lockup_light_2x_r2.png"
+          alt="logo"
+        />
+      </div>
+      <div className="header__middle">
+        <SearchIcon />
+        <input type="text" placeholder="Search mail" />
+        <ArrowDropDown className="header__inputCaret" />
+      </div>
+      <div className="header__right">
+        <IconButton>
+          <AppsIcon />
+        </IconButton>
+        <IconButton>
+          <Notifications />
+        </IconButton>
+
+        <Avatar />
+      </div>
     </div>
   );
 }
